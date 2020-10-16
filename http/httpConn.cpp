f8a6914c6873078c57211cb4fbfb476870f87bc1
ShenCastle <554780922@qq.com>
2020-10-16 18:00:01 +0800

    完成http对请求报文的解析

--- http/httpConn.cpp
+++ http/httpConn.cpp
@@ -1,6 +1,130 @@
 #include "httpConn.h"
 
 
+void HttpConn::process() {
+    HTTP_CODE readRes = processRead();
+    if (readRes == NO_REQUEST) {
+        modfd(epollfd, sockfd, EPOLLIN);
+        return;
+    }
+    bool writeRes = processWrite(readRes);
+    if (!writeRes) {
+        closeConn();
+    }
+    modfd(epollfd, sockfd, EPOLLOUT);
+}
+HttpConn::HTTP_CODE HttpConn::processRead() {
+    LINE_STATUS lineStatus = LINE_OK;
+    HTTP_CODE res = NO_REQUEST;
+    std::string text = "";
+    while ((checkState == CHECK_STATE_CONTENT && lineStatus == LINE_OK) || (lineStatus = parseLine()) == LINE_OK) {
+        text = std::string(readBuffer + startLine);
+        checkedIdx = startLine;
+        switch (checkState) {
+            case CHECK_STATE_REQUESTLINE:
+                res = parseReqLine(text);
+                if (res == BAD_REQUEST) {
+                    return BAD_REQUEST;
+                }
+                break;
+            case CHECK_STATE_HEADER:
+                res = parseHeader(text);
+                if (res == BAD_REQUEST) {
+                    return BAD_REQUEST;
+                }
+                else if (res == GET_REQUEST) {
+                    return handleReq();
+                }
+                break;
+            case CHECK_STATE_CONTENT:
+                res = parseBody(text);
+                if (res == GET_REQUEST) {
+                    return handleReq();
+                }
+                lineStatus = LINE_UNFIN;
+                break;
+            default:
+                return INTERNAL_ERROR;
+        }
+    }
+    return NO_REQUEST;
+}
+HttpConn::LINE_STATUS HttpConn::parseLine() {
+    char c;
+    for (; checkedIdx < readIdx; checkedIdx++) {
+        c = readBuffer[checkedIdx];
+        if (c == '\r') {
+            if (checkedIdx == readIdx - 1) {
+                return LINE_UNFIN;
+            }
+            else if (readBuffer[checkedIdx + 1] == '\n') {
+                readBuffer[checkedIdx++] = '\0';
+                readBuffer[checkedIdx++] = '\0';
+                return LINE_OK;
+            }
+            return LINE_BAD;
+        }
+        else if (c == '\n') {
+            if (checkedIdx > 0 && readBuffer[checkedIdx - 1] == '\r') {
+                readBuffer[checkedIdx - 1] = '\0';
+                readBuffer[checkedIdx++] = '\0';
+                return LINE_OK;
+            }
+            return LINE_BAD;
+        }
+    }
+    return LINE_UNFIN;
+}
+HttpConn::HTTP_CODE HttpConn::parseReqLine(const std::string& text) {
+    std::regex pattern("^([^ ]*) ([^ ]*) ([^ ]*)$");
+    std::smatch matchRes;
+    if (std::regex_match(text, matchRes, pattern)) {
+        if (matchRes[1] == "GET") {
+            method = GET;
+        }
+        else if (matchRes[1] == "POST") {
+            method = POST;
+            cgi = 1;
+        }
+        else {
+            return BAD_REQUEST;
+        }
+        url = matchRes[2];
+        if (url.empty()) {
+            return BAD_REQUEST;
+        }
+        version = matchRes[3];
+        if (url != "HTTP/1.1") {
+            return BAD_REQUEST;
+        }
+        checkState = CHECK_STATE_HEADER;
+        return NO_REQUEST;
+    }
+    return BAD_REQUEST;
+}
+HttpConn::HTTP_CODE HttpConn::parseHeader(const std::string& text) {
+    if (text == "") {
+        if (header.count("Content-length")) {
+            checkState = CHECK_STATE_CONTENT;
+            return NO_REQUEST;
+        }
+        return GET_REQUEST;
+    }
+    std::regex pattern("^([^:]*): ?(.*)$");
+    std::smatch matchRes;
+    if (std::regex_match(text, matchRes, pattern)) {
+        header[matchRes[1]] = matchRes[2];
+        return NO_REQUEST;
+    }
+    return BAD_REQUEST;
+}
+HttpConn::HTTP_CODE HttpConn::parseBody(const std::string& text) {
+    if (readIdx >= (checkedIdx + std::stoi(header["Content-length"]))) {
+        reqBody = text;
+        return GET_REQUEST;
+    }
+    return NO_REQUEST;
+}
 bool HttpConn::read() {
     if (readIdx >= READ_BUFFER_SIZE) {
         return false;
@@ -30,4 +154,36 @@ bool HttpConn::read() {
         }
         return true;
     }
+}
+void HttpConn::addfd(int epollfd, int fd, bool oneShot) {
+    epoll_event event;
+    event.data.fd = fd;
+    if (trigMode == 1) {
+        event.events = EPOLLIN | EPOLLET | EPOLLRDHUP;
+    }
+    else {
+        event.events = EPOLLIN | EPOLLRDHUP;
+    }
+    if (oneShot) {
+        event.events |= EPOLLONESHOT;
+    }
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
+    int oldOp = fcntl(fd, F_GETFL);
+    int newOp = oldOp | O_NONBLOCK;
+    fcntl(fd, F_SETFL, newOp);
+}
+void HttpConn::delfd(int epollfd, int fd) {
+    epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, 0);
+    close(fd);
+}
+void HttpConn::modfd(int epollfd, int fd, int ev) {
+    epoll_event event;
+    event.data.fd = fd;
+    if (trigMode == 1) {
+        event.events = ev | EPOLLET | EPOLLONESHOT | EPOLLRDHUP;
+    }
+    else {
+        event.events = ev | EPOLLONESHOT | EPOLLRDHUP;
+    }
+    epoll_ctl(epollfd, EPOLL_CTL_MOD, fd, &event);
 }
--- http/httpConn.h
+++ http/httpConn.h
@@ -1,9 +1,12 @@
 #include <netinet/in.h>
-#include <vector>
-#include <string>
-#include <map>
 #include <sys/stat.h>
 #include <sys/uio.h>
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <regex>
+#include <string>
+#include <unordered_map>
 
 
 class HttpConn
@@ -45,57 +48,61 @@ private:
     {
         LINE_OK = 0,
         LINE_BAD,
-        LINE_OPEN
+        LINE_UNFIN
     };
     int sockfd;
     sockaddr_in address;
-    std::vector<char> readBuffer;
+    char readBuffer[READ_BUFFER_SIZE];
     int readIdx;
     int checkedIdx;
     int startLine;
-    std::vector<char> writeBuffer;
+    char writeBuffer[WRITE_BUFFER_SIZE];
     int writeIdx;
     CHECK_STATE checkState;
     METHOD method;
-    std::vector<char> fileName;
     std::string url;
     std::string version;
-    std::string host;
+    std::unordered_map<std::string, std::string> header;
+    std::string reqBody;
+    /* std::string host;
     int contentLen;
-    bool linger;
+    bool linger; */
+    std::string fileName;
     std::string fileAddr;
     struct stat fileStat;
     struct iovec iv[2];
     int ivCount;
     int cgi;
-    std::string str;
     int bytesToSend;
     int bytesHaveSent;
     std::string docRoot;
-    std::map<std::string, std::string> users;
+    std::unordered_map<std::string, std::string> users;
     int trigMode;
     int closeLog;
+    void addfd(int epollfd, int fd, bool oneShot);
+    void delfd(int epollfd, int fd);
+    void modfd(int epollfd, int fd, int ev);
     void init();
     HTTP_CODE processRead();
     bool processWrite(HTTP_CODE res);
-    HTTP_CODE parseReqLine(std::string text);
-    HTTP_CODE parseHeader(std::string text);
-    HTTP_CODE parseContent(std::string text);
+    HTTP_CODE parseReqLine(const std::string& text);
+    HTTP_CODE parseHeader(const std::string& text);
+    HTTP_CODE parseBody(const std::string& text);
     LINE_STATUS parseLine();
     HTTP_CODE handleReq();
     void unmap();
-    bool addRes(std::string format, ...);
-    bool addContent(std::string content);
-    bool addStatusLine(int status, std::string title);
+    bool addRes(const std::string& format, ...);
+    bool addContent(const std::string& content);
+    bool addStatusLine(int status, const std::string& title);
     bool addHeader(int contenLength);
     bool addContentType();
     bool addContentLength(int contentLength);
     bool addLinger();
     bool addBlankLine();
 public:
-    HttpConn(): readBuffer(READ_BUFFER_SIZE), writeBuffer(WRITE_BUFFER_SIZE), fileName(FILENAME_LEN) {}
+    HttpConn() {}
     ~HttpConn() {}
-    void init(int sockfd, const sockaddr_in &addr);
+    void init(int sockfd, const sockaddr_in& addr);
     void closeConn(bool realClose = true);
     void process();
     bool read();
