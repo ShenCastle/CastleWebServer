c72fdca67756c042753385bee930c2e042dfa439
ShenCastle <554780922@qq.com>
2020-10-22 17:11:34 +0800

    着手完成Log来实现日志系统

---
+++ log/log.h
@@ -0,0 +1,57 @@
+#include <string>
+#include <thread>
+#include <mutex>
+#include <stdarg.h>
+#include <assert.h>
+#include <sys/stat.h>
+#include "blockdeque.h"
+#include "../buffer/buffer.h"
+
+
+class Log {
+private:
+    Log();
+    virtual ~Log();
+    void AppendLogLevelTitle_(int level);
+    void AsyncWrite_();
+    static const int LOG_PATH_LEN = 256;
+    static const int LOG_NAME_LEN = 256;
+    static const int MAX_LINES = 50000;
+    const char* path_;
+    const char* suffix_;
+    int MAX_LINES_;
+    int line_cnt_;
+    int to_day_;
+    bool is_open_;
+    Buffer buff_;
+    int level_;
+    bool is_async_;
+    FILE* fp_;
+    std::unique_lock<BlockDeque<std::string>> deque_;
+    std::unique_lock<std::thread> write_thread_;
+    std::mutex mtx_;
+public:
+    void Init(int level, const char* path = "./log", 
+                const char* suffix = ".log",
+                int max_queue_capacity = 1024);
+    static Log* Instance();
+    static void FlushLogThread();
+    void Write(int level, const char* format, ...);
+    void Flush();
+    int GetLevel();
+    void SetLevel(int level);
+    bool IsOpen() const;
+};
+
+#define LOG_BASE(level, format, ...)\
+    do {\
+        Log* log = Log::Instance();\
+        if (log->IsOpen() && log->GetLevel() <= level) {\
+            log->Write(level, format, ##__VA_ARGS__);\
+            log->Flush();\
+        }\
+    } while (0);
+#define LOG_DEBUG(format, ...) do {LOG_BASE(0, format, ##__VA_ARGS__)} while (0);
+#define LOG_INFO(format, ...) do {LOG_BASE(1, format, ##__VA_ARGS__)} while (0);
+#define LOG_WARN(format, ...) do {LOG_BASE(2, format, ##__VA_ARGS__)} while (0);
+#define LOG_ERROR(format, ...) do {LOG_BASE(3, format, ##__VA_ARGS__)} while (0);
