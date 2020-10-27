46c127027a4b9c443bdc246b919a8e3e0ef1a387
ShenCastle <554780922@qq.com>
2020-10-27 15:39:24 +0800

    完成SqlConnPool和SqlConnRAII

---
+++ pool/sqlconnpool.cpp
@@ -0,0 +1,83 @@
+#include "sqlconnpool.h"
+using namespace std;
+
+
+SqlConnPool::~SqlConnPool() {
+    Close();
+}
+
+SqlConnPool* SqlConnPool::Instance() {
+    static SqlConnPool conn_pool;
+    return &conn_pool;
+}
+
+void SqlConnPool::Init(const char* host, int port,
+                       const char* user, const char* password,
+                       const char* db, int conn_size) {
+    assert(conn_size > 0);
+    for (int i = 0; i < conn_size; i++) {
+        MYSQL* sql = nullptr;
+        sql = mysql_init(sql);
+        if (!sql) {
+            LOG_ERROR("MySql init error!");
+            assert(sql);
+        }
+        sql = mysql_real_connect(sql, host, user, password, db, port, nullptr, 0);
+        if (!sql) {
+            LOG_ERROR("MySql Connect error!");
+        }
+        conn_queue_.push(sql);
+    }
+    max_conn_ = conn_size;
+    sem_init(&sem_, 0, max_conn_);
+}
+
+void SqlConnPool::Close() {
+    lock_guard<mutex> locker(mtx_);
+    while (!conn_queue_.empty()) {
+        MYSQL* sql = conn_queue_.front();
+        conn_queue_.pop();
+        mysql_close(sql);
+    }
+    mysql_library_end();
+}
+
+MYSQL* SqlConnPool::GetConn() {
+    MYSQL* sql = nullptr;
+    if (conn_queue_.empty()) {
+        LOG_WARN("SqlConnPool is busy!");
+        return nullptr;
+    }
+    sem_wait(&sem_);
+    {
+        lock_guard<mutex> locker(mtx_);
+        sql = conn_queue_.front();
+        conn_queue_.pop();
+    }
+    return sql;
+}
+
+void SqlConnPool::FreeConn(MYSQL* sql) {
+    assert(sql);
+    lock_guard<mutex> locker(mtx_);
+    conn_queue_.push(sql);
+    sem_post(&sem_);
+}
+
+int SqlConnPool::GetFreeConnCount() {
+    lock_guard<mutex> locker(mtx_);
+    return conn_queue_.size();
+}
+
+SqlConnRAII::SqlConnRAII(MYSQL** sql, SqlConnPool* connpool) {
+    assert(connpool);
+    *sql = connpool->GetConn();
+    sql_ = *sql;
+    connpool_ = connpool;
+}
+
+SqlConnRAII::~SqlConnRAII() {
+    if (sql_) {
+        connpool_->FreeConn(sql_);
+    }
+}
---
+++ pool/sqlconnpool.h
@@ -0,0 +1,35 @@
+#include <mysql/mysql.h>
+#include <queue>
+#include <mutex>
+#include <semaphore.h>
+#include "../log/log.h"
+
+
+class SqlConnPool {
+private:
+    SqlConnPool() = default;
+    ~SqlConnPool();
+    int max_conn_;
+    std::queue<MYSQL*> conn_queue_;
+    std::mutex mtx_;
+    sem_t sem_;
+
+public:
+    static SqlConnPool* Instance();
+    void Init(const char* host, int port,
+              const char* user, const char* password,
+              const char* db, int conn_size);
+    void Close();
+    MYSQL* GetConn();
+    void FreeConn(MYSQL* conn);
+    int GetFreeConnCount();
+};
+
+class SqlConnRAII {
+private:
+    MYSQL* sql_;
+    SqlConnPool* connpool_;
+public:
+    SqlConnRAII(MYSQL** sql, SqlConnPool* connpool);
+    ~SqlConnRAII();
+};
